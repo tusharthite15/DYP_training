4ed2174985a34aecf6f86fbd11b1252e1dc0c8c0
Tushar Thite <111340880+tusharthite15@users.noreply.github.com>
2024-07-04 10:40:30 +0530

    Update ArrayInsertionSorted.c with the ans

--- ArrayInsertionSorted.c
+++ ArrayInsertionSorted.c
@@ -1,24 +1,39 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int i, n, j, num, pos, arr[20];
-    printf("Enter the No. of elements:");
+
+    printf("Enter the No. of elements: ");
     scanf("%d", &n);
-    for(i=0;i<n;i++)
-    {
-        printf("\n  arr[%d]:", i);
+
+    for (i = 0; i < n; i++) {
+        printf("\narr[%d]: ", i);
         scanf("%d", &arr[i]);
-
     }
-    printf("Enter the No. to be inserted:");
+
+    printf("Enter the No. to be inserted: ");
     scanf("%d", &num);
-    // Comapre the Number with sorted array and then insert it.
-    // CODE HERE                                    
-    n = n+1;
-    printf("The array after insertion is:");
-    for(i=0; i<n;i++)
-    {
-        printf("\n arr[%d] = %d", i, arr[i]);
 
+    // Find the position to insert the new number
+    for (pos = 0; pos < n; pos++) {
+        if (arr[pos] > num) {
+            break;
+        }
     }
+
+    // Shift elements to the right to make space for the new number
+    for (j = n; j > pos; j--) {
+        arr[j] = arr[j - 1];
+    }
+
+    // Insert the new number
+    arr[pos] = num;
+    n = n + 1;
+
+    printf("The array after insertion is:");
+    for (i = 0; i < n; i++) {
+        printf("\narr[%d] = %d", i, arr[i]);
+    }
+
     return 0;
 }
